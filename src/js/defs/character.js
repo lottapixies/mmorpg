b9a47fc12a88503b9b42a95d6cfdc2e972921059
Pixie Lott <pixielott@riseup.net>
2019-05-11 01:45:30 +0300

    Add characters.

---
+++ src/js/defs/character.js
@@ -0,0 +1,20 @@
+
+mmorpg.CHARACTER = {
+	name: "",
+	handle: "",
+	faction: null,
+	class: null,
+	born: null,
+	stats: {
+		strength: 0,
+		agility: 0,
+		intellect: 0,
+		luck: 0
+	},
+	equipment: {
+
+	},
+	backpack: {
+
+	}
+}
